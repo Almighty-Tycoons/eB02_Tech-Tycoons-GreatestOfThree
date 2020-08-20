1339f04a3dbca97cbd4731e4aacfb9adc8956f6a
Aasiya-8833 <56215830+Aasiya-8833@users.noreply.github.com>
2020-08-21 05:15:17 +0530

    fix all issues

--- experiment/simulation/js/simulation_js.js
+++ experiment/simulation/js/simulation_js.js
@@ -19,7 +19,7 @@ function displayElements() {
     a = Number(a);
     b = Number(b);
     c = Number(c);
-    if (isNaN(typeof(a)) || isNaN(typeof(a)) || isNaN(typeof(a))){
+    if (isNaN(typeof(a)) || isNaN(typeof(b)) || isNaN(typeof(c))){
       alert("Please provide numbers only.");
       location.reload();
       return;
@@ -54,7 +54,7 @@ function displayElements() {
     a = Number(a);
     b = Number(b);
     c = Number(c);
-    if (isNaN(typeof(a)) || isNaN(typeof(a)) || isNaN(typeof(a))){
+    if (isNaN(typeof(a)) || isNaN(typeof(b)) || isNaN(typeof(c))){
       alert("Please provide numbers only.");
       location.reload();
       return;

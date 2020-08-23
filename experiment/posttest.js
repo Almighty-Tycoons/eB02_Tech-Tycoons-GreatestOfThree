36fdb51748d2b35f92ed30f38d843034d43c113d
Aasiya-8833 <56215830+Aasiya-8833@users.noreply.github.com>
2020-08-23 12:21:44 +0530

    Update posttest.js

--- experiment/posttest.js
+++ experiment/posttest.js
@@ -90,29 +90,59 @@
 
   const myQuestions = [
     {
-      question: "This is question 1?",  ///// Write the question inside double quotes
+      question: "Which of the following is the correct relational operator?",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: " +, -, *, %, /",                  ///// Write the option 1 inside double quotes
+        b: ">, >=, <, <=, ==, !=",                  ///// Write the option 2 inside double quotes
+        c: " ++, --",                  ///// Write the option 3 inside double quotes
+        d: "&&, ||, !"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "a"                ///// Write the correct option inside double quotes
+      correctAnswer: "b"                ///// Write the correct option inside double quotes
     },
 
     {
-      question: "This is question 2?",  ///// Write the question inside double quotes
+      question: "Find the correct way of comparing three variable, checking num1 is greatest.",  ///// Write the question inside double quotes
       answers: {
-        a: "Option 1",                  ///// Write the option 1 inside double quotes
-        b: "Option 2",                  ///// Write the option 2 inside double quotes
-        c: "Option 3",                  ///// Write the option 3 inside double quotes
-        d: "Option 4"                   ///// Write the option 4 inside double quotes
+        a: "if(num1 > num2 > num3)",                  ///// Write the option 1 inside double quotes
+        b: "if(num1 > num2 == num3)",                  ///// Write the option 2 inside double quotes
+        c: "if((num1>num2) && (num1>num3))",                  ///// Write the option 3 inside double quotes
+        d: "if((num1>num2) || (num1>num3))"                   ///// Write the option 4 inside double quotes
       },
-      correctAnswer: "b"                ///// Write the correct option inside double quotes
+      correctAnswer: "c"                ///// Write the correct option inside double quotes
     },                                  ///// To add more questions, copy the section below 
     									                  ///// this line
 
 
+    {
+      question: 'int main() <br>{static int i=5; <br>if(--i){ <br>main(); <br>printf("%d ",i); }<br>} ',
+      answers: {
+        a: "0 0 0 0",
+        b: "0",
+        c: "0000",
+        d: "0 0"
+      },
+      correctAnswer: "a"
+    },
+    {
+      question: 'Output of code will be :<br>int main (){<br>int a, b;<br>a = b = 4;<br>b = a++;<br>printf ("%d %d %d %d", a++, --b, ++a, b--);}',
+      answers: {
+        a: "2020",
+        b: "6264",
+        c: "6564",
+        d: "6364"
+      },
+      correctAnswer: "b"
+    },
+    {
+      question: 'Output of code will be :<br>int main (){<br>int a = 10, b = 25;<br>a = b++ + a++;<br>b = ++b + ++a;<br>printf("%d %d ", a, b, c, d);}',
+      answers: {
+        a: "36 63",
+        b: "63 36",
+        c: "Error",
+        d: "0 0"
+      },
+      correctAnswer: "c"
+    },
     /* To add more MCQ's, copy the below section, starting from open curly braces ( { )
         till closing curly braces comma ( }, )
 
